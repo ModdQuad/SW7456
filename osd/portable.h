5f84959ee0f610299ce4ccb64f5e799db6cbf3ba
ModdQuad <58707021+ModdQuad@users.noreply.github.com>
2024-03-07 13:11:16 -0800

    Add files via upload

---
+++ osd/portable.h
@@ -0,0 +1,22 @@
+/*
+ * portable.h
+ *
+ *  Created on: Sep 24, 2019
+ *      Author: todd
+ */
+
+#ifndef PORTABLE_H_
+#define PORTABLE_H_
+
+#include "main.h"
+
+typedef int32_t  S32;
+typedef uint32_t U32;
+typedef int16_t  S16;
+typedef uint16_t U16;
+typedef int8_t   S8;
+typedef uint8_t  U8;
+
+#include "osd_config.h"
+
+#endif /* PORTABLE_H_ */
